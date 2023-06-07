fb662838ad8a83099de2ef4daad94fb78607e3f2
muhammad-nazmi12 <imuhammadnazmi@gmail.com>
2023-06-07 22:05:07 +0800

    Add env file

--- VAASDEV/settings.py
+++ VAASDEV/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 import sys
 sys.path.append('/path/to/forms')
 
@@ -86,15 +87,15 @@ WSGI_APPLICATION = 'VAASDEV.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+dotenv.load_dotenv()
 DATABASES = {
     'default':{
         'ENGINE':'django.db.backends.postgresql',
-        'NAME':'VAASDEV',
-        'USER':'VAAS',
-        'PASSWORD':'P@ssw0rd',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
+        'HOST':os.environ.get('DB_HOST'),
+        'PORT':os.environ.get('DB_PORT'),
     }
 }
 
